04cc3bf767c40fc72a82cded759191761a899e81
mduczmal <miko.duczmal@gmail.com>
2020-07-31 19:58:36 +0200

    Use Material UI on hello page

--- src/main/resources/static/js/hello.js
+++ src/main/resources/static/js/hello.js
@@ -1,16 +1,62 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
+import Button from '@material-ui/core/Button';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import { makeStyles } from '@material-ui/core/styles';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import green from '@material-ui/core/colors/green';
+import teal from '@material-ui/core/colors/teal';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: teal[500],
+        },
+        secondary: {
+            main: green[500],
+        },
+    },
+});
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+}));
+
+function TopBar(props) {
+    const classes = useStyles();
+    return (<AppBar position="static">
+        <Toolbar>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                <MenuIcon/>
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+                Terapia
+            </Typography>
+            <Button color="inherit">Login</Button>
+        </Toolbar>
+    </AppBar>
+    );
+
+}
 class Hello extends React.Component {
     render() {
         return (
-        <nav className="navbar navbar-dark bg-success">
-            <a className="navbar-brand">Terapia</a>
-            <p className="btn btn-success btn-outline-light mx-auto">Hello, React!</p>
-        </nav>
-    )
+            <MuiThemeProvider theme={theme}>
+                <TopBar/>
+            </MuiThemeProvider>
+        );
     }
 }
-ReactDOM.render(
-<Hello />,
-    document.getElementById('react')
-);
+ReactDOM.render( <Hello />, document.getElementById('react') );
