80ad0031506d7eee6329ac015ea94dfa5fc5e6b3
mduczmal <miko.duczmal@gmail.com>
2020-11-12 09:48:41 +0100

    Remove useless class

--- src/main/java/com/mduczmal/therapy/HelloController.java
+++ src/main/java/com/mduczmal/therapy/HelloController.java
@@ -1,11 +1,9 @@
 package com.mduczmal.therapy;
 
-import com.mduczmal.therapy.user.Person;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Collections;
@@ -25,16 +23,4 @@ public class HelloController {
     public @ResponseBody Map<String, String> data() {
         return Collections.singletonMap("response", "hi, react!");
     }
-
-    @PostMapping(value = "/v2/person",
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody Map<String, String> person(@RequestBody Person person) {
-        System.out.println("Person:");
-        System.out.println(person.getName());
-        System.out.println(person.getSurname());
-        System.out.println(person.getEmail());
-        return Collections.singletonMap("result", "ok!");
-    }
-
 }
